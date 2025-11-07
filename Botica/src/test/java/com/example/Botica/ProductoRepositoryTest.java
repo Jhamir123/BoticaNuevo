a7ee3af3dee6dd312e92f44be304a4a8ad5fd9a6
Jhamir <Jhamir.email@example.com>
2025-11-06 23:55:30 -0500

    Se mejoro el test

--- Botica/src/test/java/com/example/Botica/ProductoRepositoryTest.java
+++ Botica/src/test/java/com/example/Botica/ProductoRepositoryTest.java
@@ -5,6 +5,7 @@ import com.example.Botica.repository.ProductoRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -12,6 +13,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
+@ActiveProfiles("test") // usa application-test.properties (H2)
 class ProductoRepositoryTest {
 
     @Autowired
@@ -24,13 +26,17 @@ class ProductoRepositoryTest {
         p.setDescripcion("Analgésico");
         p.setCategoria("Analgésicos");
         p.setPrecio(new BigDecimal("3.50"));
-        p.setPrecioRegular(new java.math.BigDecimal("4.20"));
+        p.setPrecioRegular(new BigDecimal("4.20"));
         p.setPromoActiva(true);
         p.setDestacado(true);
+
         productoRepository.save(p);
 
         List<Producto> all = productoRepository.findAll();
-        assertThat(all).isNotEmpty();
-        assertThat(all.get(0).getNombre()).isEqualTo("Paracetamol 500mg");
+
+        assertThat(all)
+            .isNotEmpty()
+            .extracting(Producto::getNombre)
+            .contains("Paracetamol 500mg");
     }
 }
